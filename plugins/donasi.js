c8a7f18dc8018b36cf4e13f21eb05b02b99107af
mhentisbotz <109332967+mhentisbotz@users.noreply.github.com>
2022-07-20 08:26:08 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Telkomsel [08133533848]
-│ • Gopay  [085785694474]
-│ • Dana  [08133533848]
-│ • Follow Ig owner juga termasuk donasi
-│   [https://Instagram.com/nan_sakrin]
+│ • Dana [085646606905]
+│ • Gopay  [081946945315]
+│ • Pulsa  [085646606905]
+│ • Subscribe Yt owner juga termasuk donasi
+│   [https://is.gd/4ZPpIt]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/66855048169
+│ > Ingin donasi? Wa.me/6281946945315? 
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
